9b1475cbe3ad30c873e590579857d48507309775
xin-he <xinhe@ufl.edu>
2016-02-20 09:27:48 -0600

    server update to be compatible with current client socket.io

--- server.js
+++ server.js
@@ -28,56 +28,67 @@ io.on('connection', function(socket) {
   console.log('a user connected');
   console.log(socket.id);
 
-
   // When users are on the app
   socket.on('active', function(data) {
-      currentUsers[socket.id] = data.username;
+    currentUsers[socket.id] = data.username;
   });
-
 
   // Joining and leaving rooms
   socket.on('subscribe', function(data) { 
     console.log(data.username + ' joining room ', data.room);
-    if (rooms[data.room]) {
-      console.log('joining existing room ' + rooms[data.room]);  
-    } else {
-      console.log('creating new room');
-      rooms[data.room] = [];
+    if (!rooms[data.room]) {
+      console.log('creating new room ' + data.room);
+      rooms[data.room] = [data.username];
+      socket.join(data.room);
+      // Notify others in room
+      io.sockets.in(data.room).emit('notify', {
+        "msg": data.username + " has joined!",
+        "room": data.room
+      });
+    } 
+    else if (rooms[data.room].indexOf(data.username) === -1) {
+      console.log(data.username + ' joining existing room ' + data.room);    
+      rooms[data.room].push(data.username);
+      socket.join(data.room); 
+      // Notify others in room
+      io.sockets.in(data.room).emit('notify', {
+        "msg": data.username + " has joined!",
+        "room": data.room
+      });
+    } 
+    else {
+      console.log(data.username + ' already in room ' + data.room);    
     }
-    rooms[data.room].push(data.username);
-    socket.join(data.room); 
-
-    // Notify others in room
-    io.sockets.in(data.room).emit('notify', {
-      "msg": data.username + " has joined!",
-      "room": data.room
-    });
   });
+
   socket.on('unsubscribe', function(data) {  
     console.log(data.username + ' leaving room ', data.room);
-    if (rooms[data.room]) {
+    if (!rooms[data.room]) {
+      console.log('room does not exist');
+    }
+    else if (rooms[data.room].indexOf(data.username) > -1) {
+      rooms[data.room].splice(rooms[data.room].indexOf(data.username), 1);
       if (rooms[data.room].length === 0) {
         delete rooms[data.room];
         console.log('room ' + data.room + ' deleted');
       } else {
-        if (rooms[data.room].indexOf(data.username) > -1) {
-          rooms[data.room].splice(rooms[data.room].indexOf(data.username), 1);
-        }        
-      }
+         // Notify others in room
+        io.sockets.in(data.room).emit('notify', {
+          "msg": data.username + " has left",
+          "room": data.room
+        });
+      } 
+      socket.leave(data.room);
+    }  
+    else {
+      console.log(data.username + ' not in ' + data.room);
     }
-    socket.leave(data.room); 
-
-    // Notify others in room
-    io.sockets.in(data.room).emit('notify', {
-      "msg": data.username + " has left",
-      "room": data.room
-    });
   });
 
 
   // Receiving update and broadcasting to room
   socket.on('update', function(data) {
-    if (data.username in rooms[data.room]) {
+    if (rooms[data.room].indexOf(data.username) > -1) {
       console.log(data.username + ' updating room ' + data.room);
       console.log(data);
       io.sockets.in(data.room).emit('update', data);  
@@ -86,6 +97,17 @@ io.on('connection', function(socket) {
     }
   });
 
+  // Receives chat
+  socket.on('chat', function(data) {
+    if (rooms[data.room].indexOf(data.username) > -1) {
+      console.log(data.username + ' updating room ' + data.room);
+      console.log(data);
+      socket.broadcast.to(data.room).emit('chat', data);  
+    } else {
+      console.log(data.username + ' not in room ' + data.room);
+    }
+  });
+  
   // Disconnect
   socket.on('disconnect', function() {
     delete currentUsers[socket.id];
